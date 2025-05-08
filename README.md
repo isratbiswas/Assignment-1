6041adc622ed8a24ba12b6bcff8307a813d03d66
Israt Jahan <isratbiswas28@gmail.com>
2025-05-08 23:33:54 +0600

    write a blog

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Assignment-1
+1. What are some differences between interfaces and types in TypeScript?
+   Ans: Interfaces and types are similar, but they primarily apply to object types.
+   We use interfaces to declare and define the shape of objects. Interfaces are powerful tools for describing the structure of object types.
+   Example:
+   interface Person {
+   name: string;
+   age: number;
+   profession: string;
+   gender: string;
+   }
+
+Types, on the other hand, can be used for both primitive types such as string, number, and boolean, as well as more complex types like objects and arrays.
+Types are useful for improving code readability and can be easily used for type declarations.
+Example:
+type UserName = string
+type IsAdmin =boolean
+const userName : UserName = "Israt"
+const isAdmin : IsAdmin = true
+
+2.What is the use of the keyof keyword in TypeScript? Provide an example.
+
+Ans: keyof is a keyword in TypeScript used to extract the key types from an object type.
+When used with an explicit object type, keyof creates a union type of all the keys in that object.
+Example:
+type Student ={
+id:number,
+name: string,
+email: string,
+address: string,
+ContactNo: number
+}
+type Student1= keyof Student
+const person : Student1 = "email
---
+++ assignment.ts
@@ -0,0 +1,112 @@
+// problem-1
+
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
+}
+
+//  problem -2
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+//  problem-3
+type GenericArray<T> = Array<T>;
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  const result: T[] = [];
+  for (const array of arrays) {
+    result.push(...array);
+  }
+  return result;
+}
+
+//problem- 4
+class Vehicle {
+  private make: string;
+  private year: number;
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  public getInfo(): string {
+    return `Make: ${this.make}, Year: ${this.year} `;
+  }
+}
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+//problem-5
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (typeof value === "number") {
+    return value * 2;
+  } else {
+    throw new Error("please give a string or number");
+  }
+}
+
+// problem-6
+
+interface Product {
+  name: string;
+  price: number;
+}
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  }
+  let mostExpensive = products[0];
+  for (let i = 1; i < products.length; i++) {
+    if (products[i].price > mostExpensive.price) {
+      mostExpensive = products[i];
+    }
+  }
+  return mostExpensive;
+}
+
+// problem -7
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  switch (day) {
+    case Day.Saturday:
+    case Day.Sunday:
+      return "Weekend";
+    default:
+      return "Weekday";
+  }
+}
+
+// problem- 8
+
+async function squareAsync(n: number): Promise<number> {
+  if (n < 0) {
+    throw new Error("Negative number not allowed");
+  }
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return n * n;
+}
